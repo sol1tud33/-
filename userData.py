ccb443fb41802800a3513a068af69f025492f65c
sol1tud33 <95398207+sol1tud33@users.noreply.github.com>
2023-03-15 19:08:13 +0800

    Add files via upload

---
+++ userData.py
@@ -0,0 +1,6 @@
+user_data = [{
+	'Phone': '17314419220',
+	'PassWord': '20021006CXG',
+	'SendKey': 'SCT173046TB3TT1kMWD2m8CZHeSzkg0Cda', # 用于微信通知, 在Server酱官网注册用户获取
+	'Address': '{"Reason": "", "AttachmentFileName": "", "LngLat": "118.74982,31.957315","Address": "江苏省南京市雨花台区铁心桥街道三江学院主校区四号食堂"}'
+}]
